1aada4b776cdc8aedc3236a9d778f61d06d01423
Prakhar Singh <singhprakhar973@gmail.com>
2021-04-21 01:43:25 +0530

    Footer Created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './Components/navbar'
 import Login from './Components/login'
+import Footer from './Components/footer'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Login />
+      <Footer />
     </div>
   );
 }
